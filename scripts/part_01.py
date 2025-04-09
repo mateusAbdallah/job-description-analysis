6b15723a62763c5b62eebbe3bb6bd97e21d1dab3
Mateus Abdallah <abdallahmateus@gmail.com>
2025-04-08 23:44:52 -0700

    clean dataset

--- scripts/part_01.py
+++ scripts/part_01.py
@@ -4,9 +4,47 @@ import pandas as pd
 
 # %%
 
-df = pd.read_csv('../data/google_jobs_api.csv')
-df.shape
+df_da = pd.read_csv('../data/data_analyst.csv')
+df_da.head()
+
+# %%
+
+df_ds = pd.read_csv('../data/data_scientist.csv')
+df_ds.head()
 
 # %% 
 
-df.info(memory_usage='deep')
+columns_dropped_da = ['Unnamed: 0', 'Salary Estimate',
+       'Rating', 'Company Name', 'Headquarters', 'Size', 'Founded',
+       'Type of ownership', 'Industry', 'Sector', 'Revenue', 'Competitors',
+       'Easy Apply'
+       ]
+
+df_da = df_da.drop(columns=columns_dropped_da)
+
+# %%
+
+columns_dropped_ds = ['Unnamed: 0', 'index', 'Salary Estimate',
+        'Rating', 'Company Name',  'Headquarters',
+       'Size', 'Founded', 'Type of ownership', 'Industry', 'Sector', 'Revenue',
+       'Competitors', 'Easy Apply'
+       ]
+
+
+df_ds = df_ds.drop(columns=columns_dropped_ds)
+
+# %%
+
+df_concat = pd.concat([df_da, df_ds])
+
+# %%
+
+df_concat = (df_concat.rename(columns={'Job Title': 'job_title', 
+                                      'Job Description': 'job_description', 
+                                      'Location': 'location'})
+                       .drop_duplicates())
+                      
+
+# %%
+
+
